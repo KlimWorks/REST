06e85a8aec63d718f0d960d0538195492823ec53
KlimWorks <mostimportantemailbox@gmail.com>
2021-06-01 15:27:35 +0300

    Init Commit

---
+++ src/main/java/model/Deal.java
@@ -0,0 +1,24 @@
+
+package model;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import utils.CustomLocalDateSerializer;
+
+@Data
+@AllArgsConstructor
+@JsonPropertyOrder({"name", "date", "id"})
+
+public class Deal {
+    
+    private int id;
+    private String name;
+    
+    @JsonSerialize(using = CustomLocalDateSerializer.class)
+    private LocalDate date;
+    
+    
+}
