4e38c9c6d5b72aab3a27c9b1bf78d63479b71d36
KlimWorks <mostimportantemailbox@gmail.com>
2021-06-01 19:28:56 +0300

    JsonConvertServlet remastered

--- src/main/java/servlets/ContextListener.java
+++ src/main/java/servlets/ContextListener.java
@@ -1,10 +1,11 @@
 
 package servlets;
 
-import java.util.GregorianCalendar;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -18,6 +19,8 @@ import utils.DealUtils;
 public class ContextListener implements ServletContextListener {
     
     private Map <Integer, Deal> deals;
+    
+    private Table<String, LocalDate, Integer> dealsTable;
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
@@ -28,10 +31,14 @@ public class ContextListener implements ServletContextListener {
         deals = new ConcurrentHashMap();
         servletContext.setAttribute("deals", deals);
         
+        dealsTable = HashBasedTable.create();
+        servletContext.setAttribute("dealsTable", dealsTable);
+        
         List<Deal> dealList = DealUtils.generateDeals();
         
         dealList.forEach(deal -> this.deals.put(deal.getId(), deal));
         
+        dealList.forEach(deal -> this.dealsTable.put(deal.getName(), deal.getDate(), deal.getId()));
     }
 
     @Override
--- src/main/java/servlets/JsonConvertServlet.java
+++ src/main/java/servlets/JsonConvertServlet.java
@@ -2,8 +2,12 @@
 package servlets;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Table;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import static java.time.LocalDate.parse;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.ServletException;
@@ -18,12 +22,17 @@ public class JsonConvertServlet extends HttpServlet{
     
     private Map <Integer, Deal> deals;
     
+    private Table<String, LocalDate, Integer> dealsTable;
+    
     
     @Override
     public void init() throws ServletException {
         
         final Object deal = getServletContext().getAttribute("deals");
         this.deals = (ConcurrentHashMap<Integer, Deal>) deal;
+        
+        final Object dealTable = getServletContext().getAttribute("dealsTable");
+        this.dealsTable = (Table<String, LocalDate, Integer>) dealTable;
         
     }
 
@@ -32,11 +41,26 @@ public class JsonConvertServlet extends HttpServlet{
             throws IOException {
         
         request.setCharacterEncoding("UTF-8");
-        final String id = request.getParameter("id");
         
-        final Deal deal = deals.get(Integer.parseInt(id));
+        //final String id = request.getParameter("id"); // это было ранее
+        
+        final String name = request.getParameter("name");
+        
+        LocalDate startDate = parse(request.getParameter("start_date"));
+        final LocalDate endDate = parse(request.getParameter("end_date"));
+        
+        // final Integer deal_number = dealsTable.get(name, startDate); 
+        
+        ArrayList<Integer> anotherJsonDeal = new ArrayList<>();
+        
+        while(endDate.isAfter(startDate)){
+            anotherJsonDeal.add(dealsTable.get(name, startDate));
+            startDate = startDate.plusDays(1);
+        }
+                
+        //final Deal deal = deals.get(Integer.parseInt(id)); // это было ранее
                
-        final String jsonDeal = new ObjectMapper().writeValueAsString(deal);
+        final String jsonDeal = new ObjectMapper().writeValueAsString(anotherJsonDeal);
         
         response.setContentType("application/json; charset = UTF-8");
         PrintWriter out = response.getWriter();
